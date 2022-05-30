41b5d42ae47a6e3e0d8739032126a485e6572a7c
Alphalexis <alexis.leonarduzzi@gmail.com>
2022-05-30 03:58:43 +0200

    30/05/2022 03:58

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -12,10 +12,29 @@ const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const close = document.querySelector("span.close");
-const firstName = document.getElementById("first");
-const lastName = document.getElementById("last");
-const birthdate = document.getElementById("birthdate");
+const firstNameInput = document.getElementById("first");
+const lastNameInput = document.getElementById("last");
+const emailInput = document.getElementById("email");
+const birthdateInput = document.getElementById("birthdate");
+const quantityInput = document.getElementById("quantity");
+const locationInput = document.querySelectorAll(".checkbox-input[type=radio]");
 const checkboxInput = document.getElementById("checkbox1");
+const confirmModal = document.querySelector(".confirm-modal");
+const confirmModalClose = document.querySelector(".confirm-modal-close");
+const confirmModalMsg = document.querySelector(".confirm-modal-message");
+const confirmModalBtn = document.querySelector(".confirm-modal-btn");
+
+/* error messages that are going to be displayed for the users */
+
+const errorMessages = {
+  lastName: "Veuillez entrer un nom comportant 2 caractères ou plus.",
+  firstName: "Veuillez entrer un prénom comportant 2 caractères ou plus.",
+  email: "Veuillez entrer une adresse email valide.",
+  birthdate: "Veuillez entrer une date de naissance valide.",
+  quantity: "Veuillez entrer un nombre valide.",
+  location: "Veuillez choisir une ville.",
+  checkbox: "Veuillez accepter les conditions d'utilisations.",
+};
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -25,28 +44,155 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal form
+function closeModal() {
+  modalbg.style.display = "none";
+}
+
+
 /* You display as "none" for the parent of parent of the close button,
 which is "bground" */
 
-	close.addEventListener("click", function() {
-    this.parentElement.parentElement.style.display = 'none';
-	});
-
-  /* You set as an attribute "required" with the "required" parametre to the
-  Id requested, so that the form would be mandatory */
-
-  firstName.setAttribute("required", "required");
+close.addEventListener("click", function() {
+  this.parentElement.parentElement.style.display = 'none';
+});
 
 
-  lastName.setAttribute("required", "required");
+/*formData.setAttribute("data-error-visible=","false");*/
+
+  /* We first create an invalid alert*/
+  function isInvalid(element, message) {
+    let target;
+    if (NodeList.prototype.isPrototypeOf(element)) target = element[0].parentNode;
+    else target = element.parentNode;
+    target.setAttribute("data-error-visible", true);
+    target.setAttribute("data-error", message);
+  }
+  
+  /* same thing for the valid alert */
+  function isValid() {
+    modalbg.style.display = "none";
+    confirmModal.style.display = "flex";
+    confirmModalBtn.addEventListener("click", () => {
+      confirmModal.style.display = "none";
+    });
+    confirmModalClose.addEventListener("click", () => {
+      confirmModal.style.display = "none";
+    });
+  }
+  
 
 
-  birthdate.setAttribute("required", "required");
 
-  /* You set as an attribute "minlength" with "2" as a parametre to the
-  Id requested, so that the form would require minimum two caracters to be functional */
+/* We want to remove the default html error alerts */
+function removeAlerts() {
+	let invalidFields = document.querySelectorAll(
+		'.formData[data-error-visible="true"]'
+	);
+	for (let field of invalidFields) {
+		field.setAttribute("data-error-visible", false);
+		field.setAttribute("data-error", "");
+	}
+}
 
-  lastName.setAttribute("minlength", "2");
+
+/* this checks if the first name isn't empty and is more than 2 characters */
+
+function firstValidation() {
+	let inputValue = firstNameInput.value;
+	if (inputValue !== null && inputValue.length >= 2) return true;
+	else return false;
+}
+
+/* this checks if the last name isn't empty and is more than 2 characters */
+function lastValidation() {
+	let inputValue = lastNameInput.value;
+	if (inputValue !== null && inputValue.length >= 2) return true;
+	else return false;
+}
+
+//check if email use valid formatting
+function emailValidation() {
+	let regex = /^\S+@\S+\.\S+$/;
+	return regex.test(emailInput.value);
+}
+
+//check if birthdate is valid and older than today
+function birthdateValidation() {
+	let birthdate = new Date(birthdateInput.value);
+	let today = new Date();
+	if (birthdate.toString() !== "Invalid Date") {
+		if (
+			birthdate.getDate() >= today.getDate() &&
+			birthdate.getMonth() == today.getMonth() &&
+			birthdate.getFullYear() == today.getFullYear()
+		) {
+			return false;
+		} else {
+			return true;
+		}
+	} else {
+		return false;
+	}
+}
+
+// check if quantity is a valid number
+function quantityValidation() {
+	let regex = /^[0-9]+$/;
+	return regex.test(quantityInput.value);
+}
+
+// check if user chose a location
+function locationValidation() {
+	for (let radio of locationInput) {
+		if (radio.checked === true) return true;
+	}
+	return false;
+}
+
+//check if cgu are checked
+function checkboxValidation() {
+	return checkboxInput.checked;
+}
+
+
+function validate(event) {
+	event.preventDefault();
+	let isValidInput = true;
+	removeAlerts();
+	if (!firstValidation()) {
+		isValidInput = false;
+		isInvalid(firstNameInput, errorMessages.firstName);
+	}
+	if (!lastValidation()) {
+		isValidInput = false;
+		isInvalid(lastNameInput, errorMessages.lastName);
+	}
+	if (!emailValidation()) {
+		isValidInput = false;
+		isInvalid(emailInput, errorMessages.email);
+	}
+	if (!birthdateValidation()) {
+		isValidInput = false;
+		isInvalid(birthdateInput, errorMessages.birthdate);
+	}
+	if (!quantityValidation()) {
+		isValidInput = false;
+		isInvalid(quantityInput, errorMessages.quantity);
+	}
+	if (!locationValidation()) {
+		isValidInput = false;
+		isInvalid(locationInput, errorMessages.location);
+	}
+	if (!checkboxValidation()) {
+		isValidInput = false;
+		isInvalid(checkboxInput, errorMessages.checkbox);
+	}
+	if (isValidInput) {
+		isValid();
+	}
+}
+
   
 
 
